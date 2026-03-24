1b7b2873788d66f9c9a11ec71b4a85120a641fdd
Sunder Gandhi Ningappa <sundergandhi123@gmail.com>
2026-03-25 01:35:43 +0530

    Third Commit

---
+++ script.js
@@ -0,0 +1,76 @@
+let userScore=0;
+let compScore=0;
+
+
+const userScorepara=document.querySelector("#YS");
+const compScorepara=document.querySelector("#CS")
+const choice=document.querySelectorAll(".Choice")
+const msg=document.querySelector("#Msg");
+
+
+
+const generateCompChoice=()=>{
+        const options=["Rock","Scissor","Paper"];
+        const randomIdx=Math.floor(Math.random()*3); 
+        return options[randomIdx];
+
+}
+
+const draw=()=>{
+    console.log("The game was draw!");
+    msg.innerText="DRAW - PLay Again!!";
+    msg.style.backgroundColor='#262626'
+}
+
+const showWinner=(userWin,userChoice,compChoice)=>{
+    if (userWin===true){
+        userScore++;
+        console.log("You WIN!!");
+        userScorepara.innerText=userScore;
+        msg.innerText=`You WON! ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor="green";
+    }
+    else{
+        console.log("You LOST!!");
+        compScore++;
+        msg.innerText=`You LOSE! ${compChoice} beats ${userChoice}`;
+        compScorepara.innerText=compScore;
+
+        msg.style.backgroundColor="Red";
+    }
+
+}
+
+const playGame=(userChoice)=>{
+    console.log("Your Choice is",userChoice);
+    //Generate computer Choice
+    const compChoice= generateCompChoice();
+    console.log("computer Choice is : ",compChoice);
+
+    if( userChoice===compChoice){
+
+        draw();
+    }
+    else{
+        let userWin=true; 
+        if(userChoice=="Rock"){
+             userWin=compChoice==="Paper" ? false : true;
+        }else if(userChoice=="Paper"){
+            userWin=compChoice==="Scissor" ? false : true;
+        }
+        else{
+            userWin=compChoice==="Rock" ?false : true;   
+        }
+        showWinner(userWin);
+    }
+    
+
+}
+
+choice.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+         const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+
+    })
+})
